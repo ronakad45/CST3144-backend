89aede69db189ee28ace8408d7e15d6f78a2c8ac
Ronak Dattani <ronakad17@gmail.com>
2025-11-26 01:03:16 +0400

    logger and static middleware functionality

--- server.js
+++ server.js
@@ -16,3 +16,14 @@ MongoClient.connect('mongodb+srv://ronakad45:eOdYpsUA4uKbQ4XR@cluster0.vat1b.mon
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Logger Middleware logs all requests
+app.use((req, res, next) => {
+    const timestamp = new Date().toISOString();
+    console.log(`[${timestamp}] ${req.method} ${req.url}`);
+    next();
+});
+
+// Static File Middleware
+app.use('/images', express.static(path.join(__dirname, 'static')));
+
